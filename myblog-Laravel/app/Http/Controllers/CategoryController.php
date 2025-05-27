1478f1575e004607c7b53273c7166f549cb89440
RodriVelo <150716061+RodriVelo@users.noreply.github.com>
2025-05-27 20:56:01 -0300

    CONTROLADORES PA

---
+++ myblog-Laravel/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function getIndex() {
+        return view('category.index');
+    }
+
+    public function getShow($id) {
+        return view('category.show');
+    }
+
+    public function getCreate() {
+        return view('category.create');
+    }
+
+    public function getEdit($id) {
+        return view('category.edit');
+    }
+}
--- myblog-Laravel/routes/web.php
+++ myblog-Laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,26 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'getHome']);
 
 Route::get('/login', function () {
     return view('auth.login');
 });
 
-Route::get('/category', function () {
-    return view('category.index');
-});
+Route::get('/category', [CategoryController::class, 'getIndex']);
 
-Route::get('/category/show/{id}', function ($id) {
-    return view('category.show', compact('id'));
-});
+Route::get('/category/show/{id}', [CategoryController::class, 'getShow']);
 
-Route::get('/category/create', function () {
-    return view('category.create');
-});
+Route::get('/category/create', [CategoryController::class, 'getCreate']);
 
-Route::get('/category/edit/{id}', function ($id) {
-    return view('category.edit', compact('id'));
-});
+Route::get('/category/edit/{id}', [CategoryController::class, 'getEdit']);
+
